70ed85993f0f180b54a451d088cda8f7ad306b1a
ksenia 1234567890 <eurosetv3.04.05.14@gmail.com>
2023-05-02 20:39:46 +0300

    Добавьте файлы проекта.

---
+++ Самая полезная программа в мире/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Самая_полезная_программа_в_мире
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
